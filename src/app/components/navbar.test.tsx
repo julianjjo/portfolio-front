59ba8f56cd166a70a470797a5215d81b8175c906
Julian Mican <julian.mican@gmail.com>
2024-04-06 21:26:32 -0500

    Add test for navbar

--- src/app/components/navbar.test.tsx
+++ src/app/components/navbar.test.tsx
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event';
 import Navbar from './navbar'
  
 describe('Navbar', () => {
@@ -42,4 +43,19 @@ describe('Navbar', () => {
  
     expect(navbar).toBeVisible()
   })
+
+  it('shows the XMarkIcon in mobile view when the menu is open', () => {
+    render(<Navbar pathUrl='/' />);
+    
+    // Simulate the mobile view by ensuring the screen width is within the mobile range
+    // Note: This step may vary based on your actual media query breakpoints
+    global.innerWidth = 480; // Example mobile screen width
+    global.dispatchEvent(new Event('resize'));
+    
+    // Open the mobile menu
+    const menuButton = screen.getByRole('button');
+    userEvent.click(menuButton);
+    
+    expect(menuButton).toBeVisible();
+  });
 })
--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -27,11 +27,11 @@ export default function Navbar({ pathUrl }: { pathUrl: string }) {
                         <div className="relative flex h-16 items-center justify-between">
                             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                                 {/* Mobile menu button*/}
-                                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                <Disclosure.Button role='button' className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                                     <span className="absolute -inset-0.5" />
                                     <span className="sr-only">Open main menu</span>
                                     {open ? (
-                                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                                        <XMarkIcon data-testid="xmark-icon" className="block h-6 w-6" aria-hidden="true" />
                                     ) : (
                                         <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
                                     )}
